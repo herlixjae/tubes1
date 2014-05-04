e78fcde47c39c53cc265103704a4bac888875efc
Herli Herdiaman <herlixjae@hotmail.com>
2014-05-04 18:58:36 +0700

    Update portalboladb.sql

--- portalboladb.sql
+++ portalboladb.sql
@@ -195,6 +195,7 @@ CREATE TABLE IF NOT EXISTS `user_cat` (
   PRIMARY KEY (`id_cat_user`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
+
 --
 -- Dumping data for table `user_cat`
 --
